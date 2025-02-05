6563c303e577bdff435a2466989d83507087fc8a
“Marcos” <marcos.wiendl@gmail.com>
2025-02-05 01:46:08 -0300

    edição do texto de instrução

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={styles.App}>
       <h1>Criar Elemento</h1>
-      <h2>Utilize a ferramenta abaixo para criar dados internos de eventos. O código gerado deve ser colado no campo "detalhes" do Google Agenda.</h2>
+      <h2>Utilize a ferramenta abaixo para complementar os dados do evento. Cole o código gerado diretamente na descrição do evento dentro do Google Agenda.</h2>
       <FormularioEvento />
     </div>
   );
